8e0771e1bc65424228c4e397c4b3a0324c4418e4
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-10-09 23:17:12 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expert Pest Control Services in London</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0px 0px 15px 1px rgba(0, 0, 0, 0.2);
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 16px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        echo '<h1>Expert Pest Control Services in London</h1>';
+        echo '<p>Looking for trusted pest control services in London? <a href="https://bugwisepestcontrol.co.uk/" target="_blank">Bugwise Pest Control</a> is your go-to solution. With seasoned exterminators, Bugwise effectively handles various pest issues, from rodents to insects. Whether it\'s for residential or commercial needs, the company offers prompt and discreet emergency pest removal services, even on Bank Holidays. The technicians at Bugwise are BPCA-certified, ensuring competent and reliable pest control solutions tailored for your specific needs.</p>';
+        ?>
+        <div>
+            <a href="https://bugwisepestcontrol.co.uk/"><button>Learn More About Bugwise</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
